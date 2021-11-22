7fbef8d11baa78d2bea81fc2e00a725929f41d98
gardnertemkin <temkmg20@wfu.edu>
2021-11-22 11:28:37 -0500

    Initial commit of Text Adventure Game

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Random rand = new Random();
+    }
+}
