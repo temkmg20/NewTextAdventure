16321790771099f785054ecb9ca554704c76707f
gardnertemkin <temkmg20@wfu.edu>
2021-11-30 22:10:51 -0500

    Updates V1: Gardner

--- src/Main.java
+++ src/Main.java
@@ -12,22 +12,76 @@ public class Main {
         Random rand = new Random();
 
         // Game variables
-        String [] characters = {"Ninja", "Warrior", "Guard", "Assassin"};
+        String [] enemies = {"Ninja", "Warrior", "Guard", "Assassin"};
         int maxEnemyHealth= 100;
         int enemyHitDamage= 20;
 
         // Player variables
         int health = 100;
         int shieldValue= 15;
-        int lives = 3;
+        int numShield = 2;
+        int attackDamage = 30;
         int powerUpChance= 22; // percentage
-        String [] powerUp = {"Shield", "Double Damage", "Freeze", "Gambler"};
+        int shieldChance= 30;
+        String [] powerUp = {"Shield", "Double Damage", "Freeze"};
+
 
         boolean running = true;
         GAME:
         while (running){
             System.out.println("Welcome to Jungle Survivor\nDo you have what it takes to" +
                     " fight your way to the hidden treasure?");
+            System.out.println("Let the games begin");
+            int enemyHealth = rand.nextInt(100);
+            String enemy = enemies[rand.nextInt(enemies.length)];
+            System.out.println(enemy + " has appeared!\n");
+
+            while(enemyHealth > 0) {
+                System.out.println("Your Health: " + health);
+                System.out.println("Enemy Health: " + enemyHealth);
+                System.out.println("\nWhat would you like to do?");
+                System.out.println("1 to Attack ");
+                System.out.println("2 to Drink shield ");
+                System.out.println("3 to Use power up");
+                System.out.println("4 to Run");
+
+                int input = scnr.nextInt();
+                if (input == 1){
+                int damageDealt = rand.nextInt(attackDamage);
+                int damageTaken= rand.nextInt(enemyHitDamage);
+                enemyHealth-= damageDealt;
+                health -= damageTaken;
+                    System.out.println("You strike " + enemy + "for" + damageDealt + "damage");
+                    System.out.println("You receive " + damageTaken + " in return");
+                    if(health < 1 ){
+                        System.out.println("You have taken to much damage to go on!");
+                        break;
+                    }
+                }
+                else if (input == 2){
+                if (numShield > 0){
+                    health+= shieldValue;
+                    numShield--;
+                    System.out.println("You drank shield potion, you now have " + health + " health" +
+                            "\nYou now have" + numShield + "shield potion left");
+                }
+                else {
+                    System.out.println("You have no shield potion left! Defeat the enemy for a chance at " +
+                            "a power up or shield potion!");
+                }
+                }
+                else if (input == 3){
+
+                }
+                else if (input == 4){
+
+                }
+                else {
+                }
+
+
+            }
+
             
 
         }
---
+++ src/Ninja.java
@@ -0,0 +1,11 @@
+public class Ninja {
+private String name;
+private double health;
+private String [] options = {"Fists", "Sword"};
+
+    public Ninja (String name, double health, String [] options){
+    }
+
+    Ninja n1 = new Ninja("Ninja", 100, options);
+
+}
